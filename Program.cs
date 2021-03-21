5c1769cb811c1c2055b599edb76d86263ae9f2b4
afifahfq <afifahfq@gmail.com>
2021-03-21 23:08:58 +0700

    ctor masih eror

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tubes_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var G = new Graph(4);
+            G.AddEdge(0,2);
+            G.Display();
+        }
+    }
+}
